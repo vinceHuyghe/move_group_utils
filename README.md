0e13e3a50b98da4cbd91d9fda056bbb8362d5a46
vinceH <vinceHuyghe@users.noreply.github.com>
2022-11-24 16:59:15 +0100

    test

---
+++ README.md
@@ -0,0 +1,12 @@
+# Move group utils
+
+Convenience pkg for interfacing with move group.
+
+## pilz_robot_prog
+
+Based on the following [pkg](https://github.com/rivelinrobotics/move_group_sequence) which is in turn based on the [pilz_robot_programming](https://github.com/PilzDE/pilz_industrial_motion/tree/melodic-devel/pilz_robot_programming) but adapted to work with any manipulator with a moveit configuration. Requires the [Pilz Industrial Motion Planner](https://ros-planning.github.io/moveit_tutorials/doc/pilz_industrial_motion_planner/pilz_industrial_motion_planner.html) to be installed.
+
+
+## example usage
+
+examples with ur10e can be found [here](https://github.com/vinceHuyghe/ur10e_examples)
---
+++ scripts/test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from math import pi
+
+from geometry_msgs.msg import Point, Pose, Quaternion
+from move_group_utils.move_group_utils import MoveGroupUtils
+from move_group_utils.move_group_utils import Circ, Lin, Ptp, from_euler
+
+
+def robot_program():
+
+    mgi = MoveGroupUtils()
+
+    start = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    pose_l = Pose(position=Point(1.6, -0.6, 1.0),
+                  orientation=from_euler(0.0, pi, 0.0))
+    pose_r = Pose(position=Point(1.6, 0.6, 1.0),
+                  orientation=from_euler(0.0, pi, 0.0))
+
+    poses = [start, pose_l, pose_r]
+
+    for pose in poses:
+        mgi.sequencer.plan(Ptp(goal=pose, vel_scale=0.3, acc_scale=0.3))
+        mgi.sequencer.execute()
+
+
+if __name__ == "__main__":
+
+    robot_program()
